8837ab273d61ef2540b0ae7253b9ee934f7afc84
piyush-kashyap_psl <piyush_kashyap@persistent.com>
2024-02-26 04:42:16 -0800

    modifying AFD script

--- tests/test_ActivatingFailedDisk.py
+++ tests/test_ActivatingFailedDisk.py
@@ -16,10 +16,10 @@ def Activating_failed_disk():
 	fid = subprocess.check_output(FAILED_DISK_ID, shell=True,encoding='utf-8',universal_newlines=False).strip()
 	output = 'nzhw activate -id {i} -local'.format(i=fid)
 
-	result = subprocess.run(output, shell=True,encoding='utf-8',stdout=subprocess.PIPE,stderr=subprocess.PIPE)
-	output = result.stderr.strip()
-	expected_error_starts_with = "Error: cannot activate disk"
-	#assert output.startswith(expected_error_starts_with)
-	print(output)
-	assert 1
-	
+    result = subprocess.run(output, shell=True,encoding='utf-8',stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output = result.stderr.strip()
+
+    expected_error_starts_with = "Error: cannot activate disk"
+
+    assert output.startswith(expected_error_starts_with)
+    print(output)
