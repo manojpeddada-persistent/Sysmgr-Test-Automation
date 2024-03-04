3b25bd6e863aa5b80fc5ad79fa40b8eaa57161e3
piyush-kashyap_psl <piyush_kashyap@persistent.com>
2024-03-04 02:55:31 -0800

    modifying AFD script

--- tests/test_ActivatingFailedDisk.py
+++ tests/test_ActivatingFailedDisk.py
@@ -14,7 +14,7 @@ def Activating_failed_disk():
 	#Activating failed disk
 	FAILED_DISK_ID='nzhw -issues -local| grep -w Failed | awk -F " " \' {print$2} \' | tail -n 1'
 	fid = subprocess.check_output(FAILED_DISK_ID, shell=True,encoding='utf-8',universal_newlines=False).strip()
-	output = 'nzhw activate -id {i} -local'.format(i=fid)
+	os.system('nzhw activate -id {i} -local'.format(i=fid))
 
 	OP='nzhw -type Disk -local | awk -F " " \'{print$4}\' | awk \'NR>2{print$1}\' | tail -n 1'
 	res= subprocess.check_output(OP,shell=True,encoding='utf-8',universal_newlines=False).strip()
