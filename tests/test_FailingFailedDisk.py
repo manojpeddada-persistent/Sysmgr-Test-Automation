efcb02b1a7d9bb6af32f72341759f9f5d37748bb
piyush-kashyap_psl <piyush_kashyap@persistent.com>
2024-03-04 02:45:34 -0800

    modifying AFD script

--- tests/test_FailingFailedDisk.py
+++ tests/test_FailingFailedDisk.py
@@ -11,9 +11,9 @@ def test_failing_failed_disk():
 	time.sleep(15)
 
 	#failing failed disk
-	#FAILED_DISK_ID='nzhw -issues -local| grep -w Failed | awk -F " " \' {print$2} \' '
-	#fid = subprocess.check_output(FAILED_DISK_ID, shell=True,encoding='utf-8',universal_newlines=False).strip()
-	#os.system('echo y | nzhw failover -id {i} -local'.format(i=fid))
+	FAILED_DISK_ID='nzhw -issues -local| grep -w Failed | awk -F " " \' {print$2} \' '
+	fid = subprocess.check_output(FAILED_DISK_ID, shell=True,encoding='utf-8',universal_newlines=False).strip()
+	os.system('echo y | nzhw failover -id {i} -local'.format(i=fid))
 	
 	OP='nzhw -type Disk -local | awk -F " " \'{print$4}\' | awk \'NR>2{print$1}\' | tail -n 1'
 	res= subprocess.check_output(OP,shell=True,encoding='utf-8',universal_newlines=False).strip()
