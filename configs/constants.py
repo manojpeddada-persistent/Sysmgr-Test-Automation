0aa6c655701303f7ef34c7ef14f3bbb6ae7e6188
piyush-kashyap_psl <piyush_kashyap@persistent.com>
2024-03-03 21:30:18 -0800

    modifying constants.py for AFD script

--- configs/constants.py
+++ configs/constants.py
@@ -2,11 +2,12 @@ TestArea = [ "testarea_Disk", #TestArea Disk
 	     "testarea_SPU"   #TestArea SPU
            ]  
 
-TestCases = [ "test_FailingHealthyDisk", #TestCase Failing healthy disk
-              "test_FailingFailedDisk", #TestCase Failing failed disk
-              "test_ActivatingAlreadyActiveDisk", #TestCase Activating active disk
-	      "test_FailingFailingDisk", #TestCase Failing failing disk
-	      "test_ActivatingFailedDisk", #TestCase Activating Failed disk
-	      "test_ActivatingSingleDisk", #TestCase activating Single disk
+TestCases = [ 	"test_FailingHealthyDisk", #TestCase Failing healthy disk
+              	"test_FailingFailedDisk", #TestCase Failing failed disk
+              	"test_ActivatingAlreadyActiveDisk", #TestCase Activating active disk
+	      	"test_FailingFailingDisk", #TestCase Failing failing disk
+	      	"test_ActivatingFailedDisk", #TestCase Activating Failed disk
+	      	"test_ActivatingSingleDisk", #TestCase activating Single disk
+		"test_ActivatingFailingDisk", #TestCase activating Failing disk
 	      "All" # Executing all cases
            ]
