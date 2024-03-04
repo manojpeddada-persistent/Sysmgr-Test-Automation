98e952998e3945908df0deca142370af708f997c
piyush-kashyap_psl <piyush_kashyap@persistent.com>
2024-03-03 22:56:04 -0800

    modifying AFD script

--- tests/test_ActivatingFailedDisk.py
+++ tests/test_ActivatingFailedDisk.py
@@ -14,16 +14,12 @@ def Activating_failed_disk():
 	#Activating failed disk
 	FAILED_DISK_ID='nzhw -issues -local| grep -w Failed | awk -F " " \' {print$2} \' | tail -n 1'
 	fid = subprocess.check_output(FAILED_DISK_ID, shell=True,encoding='utf-8',universal_newlines=False).strip()
-	os.system('nzhw activate -id {i} -local'.format(i=fid))
-	#output = 'nzhw activate -id {i} -local'.format(i=fid)
+	output = 'nzhw activate -id {i} -local'.format(i=fid)
 
-    	'''result = subprocess.run(output, shell=True,encoding='utf-8',stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    	output = result.stderr.strip()
-    	expected_error_starts_with = "Error: cannot activate disk"
-    	assert output.startswith(expected_error_starts_with)
-    	print(output)'''
-	OP='nzhw -type Disk -local | awk -F " " \'{print$4}\' | awk \'NR>2{print$1}\' | tail -n 1'
-	res= subprocess.check_output(OP,shell=True,encoding='utf-8',universal_newlines=False).strip()
-	assert "Active" == res
+    result = subprocess.run(output, shell=True,encoding='utf-8',stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    output = result.stderr.strip()
 
+    expected_error_starts_with = "Error: cannot activate disk"
 
+    assert output.startswith(expected_error_starts_with)
+    print(output)
